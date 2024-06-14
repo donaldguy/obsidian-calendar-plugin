b9e7c53405580c658deecd2113918b8660d28581
Donald Guy <donaldguy@hey.com>
2024-06-14 16:38:46 -0600

    Module-ize obsidian-daily-notes-interface (w/ cjs)
    This seems to work same as 1.5.10 [after clone into `.obsidian/plugins` and `npm install` OR `pnpm install` ]
    
    (the final rollup "claims" it doesn't know where to find "obsidian-daily-notes-interface" at the typescript stage; [adding a moduleResolution: node to tsconfig surprisingly no help], but everything ends up in the output main.js)
    
    Note: pins back down to 0.12.0 of obsidian-api

--- packages/daily-notes-api/rollup.config.js
+++ packages/daily-notes-api/rollup.config.js
@@ -1,24 +1,25 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
 
 import pkg from "./package.json";
 
 export default {
-  input: "src/index.ts",
+  input: "dist/mjs/index.js",
   output: {
     format: "cjs",
-    file: "dist/main.js",
+    
+    file: "dist/bundled.cjs",
+    sourcemap: "inline",
     name: pkg.name,
   },
   external: ["obsidian"],
   plugins: [
-    typescript(),
     resolve({
       browser: true,
     }),
     commonjs({
       include: "node_modules/**",
+      sourceMap: "inline"
     }),
   ],
 };
